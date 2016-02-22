45dddd45770b937b782d616b7cd8696f12181c2e
Jude Taylor <me@jude.bio>
2016-02-21 19:08:33 -0800

    gitignore

--- shell.nix
+++ shell.nix
@@ -1,1 +1,32 @@
-../reflex-platform/shell.nix
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghcjs" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, Cabal, nodejs, reflex-dom, stdenv, time
+      , transformers
+      }:
+      mkDerivation {
+        pname = "crush-crush";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base Cabal reflex-dom time transformers
+        ];
+        buildTools = [ nodejs ];
+        description = "Date catgirls!";
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
