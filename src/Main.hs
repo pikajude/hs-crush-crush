1eb9ba21339d902ca56a0f9dd5ad61354ad4b254
Jude Taylor <me@jude.bio>
2016-02-24 17:19:29 -0800

    use scalars instead of Game -> Game modifiers

--- src/Main.hs
+++ src/Main.hs
@@ -9,33 +9,35 @@ import Control.Lens
 import Control.Monad
 import Control.Monad.Fix
 import Control.Monad.IO.Class
-import Data.Map               (Map, showTree)
+import Data.Map               (Map, fromList, showTree, unionWith)
+import Data.Monoid
 import Data.Time
+import Debug.Trace
 import Reflex.Dom
 
 data Game = Game
-          { _money :: Double
-          , _gifts :: Map String Integer
+          { _money      :: Double
+          , _gifts      :: Map String (Sum Integer)
+          , _multiplier :: Integer
           } deriving Show
 
 makeLenses ''Game
 
-ticker :: MonadWidget t m => Double -> m (Event t (Game -> Game))
+ticker :: MonadWidget t m => Double -> m (Event t Double)
 ticker fact = do
     t <- liftIO getCurrentTime
     tck <- tickLossy (1 / 60) t
     be <- hold Nothing (fmap Just tck)
-    return $ fmap ((money +~)
-        . maybe 0 (\ (a,b) -> fromRational (toRational $ diffUTCTime (_tickInfo_lastUTC b) (_tickInfo_lastUTC a)) * fact)
-        . uncurry (liftM2 (,)))
-        $ attach be (fmap Just tck)
+    return $ fmap (maybe 0 (\ (a,b) -> fromRational (toRational $ diffUTCTime (_tickInfo_lastUTC b) (_tickInfo_lastUTC a)) * fact)
+                  . uncurry (liftM2 (,)))
+           $ attach be (fmap Just tck)
 
-gift :: MonadWidget t m => String -> Integer -> Dynamic t Game -> m (Event t (Game -> Game))
+gift :: MonadWidget t m => String -> Integer -> Dynamic t Game -> m (Event t (Double, Map String (Sum Integer)))
 gift nm price dg = do
     myMoney <- mapDyn (view money) dg
-    disabler <- mapDyn (\ c -> if c < fromIntegral price then [("disabled", "1")] else []) myMoney
+    disabler <- mapDyn (\ c -> fromList [("disabled", "1") | c < fromIntegral price]) myMoney
     (buyer, _) <- elDynAttr' "button" disabler $ text $ "Buy " ++ nm ++ " ($" ++ show price ++ ")"
-    return $ fmap (const ((money -~ fromIntegral price) . (gifts . at nm . non 0 +~ 1))) (domEvent Click buyer)
+    return $ fmap (const (negate (fromIntegral price), [(nm, Sum 1)])) (domEvent Click buyer)
 
 appDyn :: (MonadFix m, MonadHold t m, Reflex t) => Game -> [Event t (Game -> Game)] -> m (Dynamic t Game)
 appDyn g xs = foldDyn ($) g (mergeWith (.) xs)
@@ -53,7 +55,12 @@ myGifts = [ ("shell", 500)
 
 main :: IO ()
 main = mainWidget $ el "div" $ do
-    rec game <- appDyn (Game 0 mempty) =<< sequence (ticker 100 : map (\ (g,p) -> gift g p game) myGifts)
+    rec monetaryBalance <- foldDyn (+) 0 $ mergeWith (+) [giftMoney, stateIncome]
+        stateIncome <- fmap (attachWith (\ g n -> n * fromIntegral (1 + length (view gifts g))) (current game)) (ticker 100)
+        giftCollection <- foldDyn (unionWith mappend) mempty giftThings
+        game <- combineDyn (\m g -> Game m g 1) monetaryBalance giftCollection
+        (giftMoney, giftThings) <- splitE . mergeWith (\(a,b) (c,d) -> (a + c, unionWith mappend b d))
+                               <$> mapM (\ (g,p) -> gift g p game) myGifts
     el "h3" $ do
         text "Money: "
         dynText =<< mapDyn ((show :: Integer -> String) . floor . view money) game
